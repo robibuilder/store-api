256309acc3f90675340e67e31edf228a3f00584e
Jacob Robinett <robinett258@gmail.com>
2022-03-13 21:59:22 -0500

    Testing initial apis

--- src/app.js
+++ src/app.js
@@ -1,14 +1,46 @@
+
+
 const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const batteries = [
+    {
+        id: 1,
+        brand: 'Hart',
+        name: 'Hart 20V Battery Mount'
+    },
+    { 
+        id: 2,
+        brand: 'Craftsman',
+        name: 'Craftman V20* Battery Mount'
+    },
+    {
+        id: 3,
+        brand: 'DeWalt',
+        name: 'DEWALT 20V Battery Mount'
+    },
+    { 
+        id: 4,
+        brand: 'Black & Decker',
+        name: 'Black & Decker 20V Battery Mount'
+    },
+  ];
+
+router.get('/batteries', (req, res) => {
     // res.json({
     //     'Hello': 'hi'
     // });
-    res.send("Hello");
+    res.status(200).json(batteries);
+});
+
+router.get('/batteries/:id', (req, res) => {
+    let id = req.params.id;
+    let result = batteries.find((item) => item.id == id);
+
+    res.status(200).json(result);
 });
 
 router.get('/test', (req, res) => {
